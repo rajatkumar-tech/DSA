d97eaa7562ce09561747b1c989a1eea8fd28bbb0
Rajat7ku <rajatkumar52557@gmail.com>
2026-06-12 20:52:22 +0530

    learn one more topic

---
+++ widening-conversion/Widening.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+/* 
+public class Widening {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        float num = sc.nextInt();
+        System.out.println(num);
+    }
+
+}
+    */
+
+
+// type casting conversion
+
+public class Widening{
+public static void main (String args[]){
+
+    // char ch = 'a';
+    // char ch1 = 'b';
+
+    byte ch = 2;
+    byte ch1 = 34;
+
+    float num = ch;
+    float num1 = ch1;
+
+    System.out.println(num);
+    System.out.println(num1);
+}
+}
