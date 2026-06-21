664da80906a6f53c8580f4b9f20089a292af9222
Rajat7ku <rajatkumar52557@gmail.com>
2026-06-21 20:44:34 +0530

    solve question trapping water using two pointer approach

--- trapping-rain-water-using-two-pointer-approach/trappingWater.java
+++ trapping-rain-water-using-two-pointer-approach/trappingWater.java
@@ -1,9 +1,40 @@
 import java.util.*;
 
 public record trappingWater() {
- 
-    
-    public static void main(String args[]){
-       int height[] = {4, 2, 0, 6, 3, 2, 5};
+
+    public static int trappedWater(int height[]) {
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int maxLeft = 0, maxRight = 0;
+
+        int waterLevel = 0;
+
+        if (n <= 2)
+            return 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    waterLevel += maxLeft - height[left];
+                }
+                left++;
+                
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    waterLevel += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return waterLevel;
+    }
+
+    public static void main(String args[]) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println(trappedWater(height));
     }
 }
