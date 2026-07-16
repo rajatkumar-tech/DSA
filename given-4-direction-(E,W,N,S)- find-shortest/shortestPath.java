2cce43be81c47997c8664cf7709276070288322f
RAJAT KUMAR <rajatkumar52557@gmail.com>
2026-07-16 17:48:11 +0530

    solve the solution error

--- given-4-direction-(E,W,N,S)- find-shortest/shortestPath.java
+++ given-4-direction-(E,W,N,S)- find-shortest/shortestPath.java
@@ -1,5 +1,32 @@
 
 
 public class shortestPath {
-    
+    public static double sortestPath(String str){
+        int x =0;
+        int y =0;
+
+        for(int i =0; i<str.length(); i++){
+            char directon = str.charAt(i);
+
+            if(directon == 'N'){
+                y++;
+            }else if (directon == 'S'){
+                y--;
+            }else if(directon == 'E'){
+                x++;
+            }else if(directon == 'W'){
+                x--;
+            }
+        }
+
+        double shortesDistance = Math.sqrt((x*x) + (y*y));
+
+        return shortesDistance;
+    }
+
+
+    public static void main(String args[]){
+        String str = "WNEENESENNN";
+        System.out.println(sortestPath(str));
+    }
 }
