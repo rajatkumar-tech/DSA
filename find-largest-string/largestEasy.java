2c31d3286d93b51885b972f79330c0ca0c329dd3
Rajat Kumar <rajatkumar52557@gmail.com>
2026-07-23 21:34:11 +0530

    add all the file

--- find-largest-string/largestEasy.java
+++ find-largest-string/largestEasy.java
@@ -1,5 +1,9 @@
 public class largestEasy {
 
+    // this method comparing to the word only 
+    // "banana" = first word is 'b'
+    // "mango"  = first word is 'm'  == M is greater to another word
+    // "apple" = first word is 'a' 
     public static String largest(String str[]) {
         if (str == null || str.length == 0) return "";
 
