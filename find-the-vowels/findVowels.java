71b04692e86e744e7b8ded338cfdbbfa156de6ac
Rajat Kumar <rajatkumar52557@gmail.com>
2026-07-25 23:16:20 +0530

    find the vowels

---
+++ find-the-vowels/findVowels.java
@@ -0,0 +1,24 @@
+// package find-the-vowels;
+
+public class findVowels {
+
+    public static int vowel(String str){
+        int count =1;
+        
+        for(int i=0; i< str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String args[]){
+        String str = "aaacctou";
+
+        System.out.println("All vowels = "+ vowel(str));
+    }
+    
+}
--- practice.java
+++ practice.java
@@ -4,9 +4,10 @@ public class practice {
         int count =0;
 
         for(int i =0; i<str.length(); i++){
-            char ch = str.charAt(i);
+            char ch = Character.toLowerCase(str.charAt(i));
             
             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                
                 count++;
             }
         }
@@ -15,7 +16,7 @@ public class practice {
 
 
     public static void main(String args[]){
-        String str = "aaauhom";
+        String str = "ABCOUM";
         System.out.println(loweCase(str));
     }
 }
