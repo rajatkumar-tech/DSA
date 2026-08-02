edeba315bda51221964b1785adea9905150eb621
Rajat Kumar <rajatkumar52557@gmail.com>
2026-08-02 23:24:14 +0530

    learning the oops concepts

---
+++ oops-concepts/oops.java
@@ -0,0 +1,36 @@
+public class oops{
+
+
+    
+
+    public static void main(String args[]){
+        Pen p1 = new Pen();
+        p1.setcolor("Blue");
+        System.out.println(p1.color);
+
+        p1.setTip(5);
+        System.out.println(p1.tip);
+
+        p1.color = "Orange";
+        System.out.println(p1.color);
+
+        p1.tip = 7;
+        System.out.println(p1.tip);
+    }
+
+
+    
+}
+
+class Pen {
+        String color;
+        int tip;
+
+        void setcolor(String newColor){
+            color = newColor;
+        }
+
+        void setTip(int newTip){
+            tip = newTip;
+        }
+    }
